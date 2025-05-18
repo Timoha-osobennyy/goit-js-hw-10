542029fd67a92138f09f1330354953914fe42f32
Timofii <141590304+Timoha-osobennyy@users.noreply.github.com>
2025-05-18 04:33:20 +0300

    snackbar-js

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,42 @@
+// Без імпорту iziToast, бо підключено в HTML
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const delay = Number(form.elements.delay.value);
+  const state = form.elements.state.value;
+
+  createPromise(delay, state)
+    .then((ms) => {
+      iziToast.success({
+        title: '✅ Success',
+        message: `Fulfilled promise in ${ms}ms`,
+        position: 'topRight',
+        timeout: 3000,
+      });
+    })
+    .catch((ms) => {
+      iziToast.error({
+        title: '❌ Error',
+        message: `Rejected promise in ${ms}ms`,
+        position: 'topRight',
+        timeout: 3000,
+      });
+    });
+
+  form.reset(); // очищає форму після сабміту
+});
+
+function createPromise(delay, state) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (state === 'fulfilled') {
+        resolve(delay);
+      } else {
+        reject(delay);
+      }
+    }, delay);
+  });
+}
